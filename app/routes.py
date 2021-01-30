e98c5037785561f55dfcf791795b73981acca886
Shameemudheen M P <shameemmanalaya26@gmail.com>
2021-01-30 12:11:06 +0530

    User login added

--- app/routes.py
+++ app/routes.py
@@ -69,6 +69,65 @@ def create_user():
     token = jwt.encode({'user_id': user.user_id}, app.config.get('SECRET_KEY'), algorithm="HS256")
     return jsonify({"message": "user created!", "user": output, "token":token})
 
+@app.route('/api/user')
+@token_required
+def get_all_users(current_user):
+    if not current_user.admin:
+        return jsonify({"error": "cannot perform that function!"})
+
+    print(current_user)
+
+    users = User.query.all()
+    output = []
+
+    for user in users:
+        user_data={}
+        user_data['username'] = user.username
+        user_data['user_id'] = user.user_id
+        user_data['name'] = user.name
+        user_data['email'] = user.email
+        user_data['admin'] = user.admin
+
+        output.append(user_data)
+
+    return jsonify({"users":output})
+
+
+@app.route('/api/user/<user_id>')
+@token_required
+def get_one_user(current_user, user_id):
+    user = User.query.filter_by(user_id=user_id).first()
+    if not user:
+        return jsonify({"error": "user not found!"}), 404
+
+    output = {
+        'username': user.username,
+        'user_id': user.user_id,
+        'name': user.name,
+        'email': user.email,
+        'admin': user.admin
+    }
+    return jsonify({"user": output})
+
+@app.route('/api/login')
+def login():
+    auth = request.authorization
+    if not auth or not auth['username'] or not auth['password']:
+        return make_response("could not verify", 401, {'WWW-Authenticate': 'Basic realm="login required!"'})
+    
+    user = User.query.filter_by(email=auth.username).first()
+    if not user:
+        user = User.query.filter_by(username=auth.username).first()
+        if not user:
+            return make_response("could not verify", 401, {'WWW-Authenticate': 'Basic realm="login required!"'})
+
+    if user.check_password(auth['password']):
+        token = jwt.encode({'user_id': user.user_id}, app.config.get('SECRET_KEY'), algorithm="HS256")
+        return jsonify({"token": token, "user_id": user.user_id})
+
+    return make_response("could not verify", 401, {'WWW-Authenticate': 'Basic realm="login required!"'})
+
+
 
 
 
