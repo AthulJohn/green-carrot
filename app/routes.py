34c8e3da529868f420cbc43573f18a9b355a4720
Shameemudheen M P <shameemmanalaya26@gmail.com>
2021-01-30 11:37:49 +0530

    function to check token created

--- app/routes.py
+++ app/routes.py
@@ -4,21 +4,35 @@ from app.models import User, Rating
 import uuid
 import jwt
 from functools import wraps
+from ml.predictor import predictor
 
 
+#token
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
 
-@app.route('/api/test', methods=['GET', 'POST'])
-def test():
-    '''this is to test
-    '''
-    food = {
-        'biriyani' : 85,
-        'oat meal': 25,
-        'fried rice': 100
-    }
-    if request.method == 'POST':
-        food.update(request.json)
-    return jsonify(food),200
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].replace("Bearer ", "")
+
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 401
+        
+        current_user = None
+
+        try:
+            data = jwt.decode(token, app.config.get('SECRET_KEY'), algorithms=["HS256"])
+            current_user = User.query.filter_by(user_id=data['user_id']).first()
+            if not current_user:
+                raise Exception()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+    
+    return decorated
+
 
 
 
