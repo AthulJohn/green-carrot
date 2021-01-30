309e0a6f545d33d5b3b9b3ba09906d156d56c97a
Shameemudheen M P <shameemmanalaya26@gmail.com>
2021-01-30 11:47:35 +0530

    user registration added

--- app/routes.py
+++ app/routes.py
@@ -34,5 +34,41 @@ def token_required(f):
     return decorated
 
 
+@app.route('/api/register', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    required = ['username', 'name', 'email', 'password']
+    
+    for r in required:
+        if not data.get(r):
+            return jsonify({"error": f"missing argument [{r}]"})
+
+    user = User.query.filter_by(username=data['username']).first()
+    if user: return jsonify({"error": "username is taken"})
+
+    user = User.query.filter_by(email=data['email']).first()
+    if user: return jsonify({"error": "email already registered"})
+
+    try:
+        user = User(user_id=str(uuid.uuid4()), username=data['username'], name=data['name'], email=data['email'], admin=False)
+        user.set_password_hash(data['password'])
+        db.session.add(user)
+        db.session.commit()
+    except AssertionError as e:
+        msg = str(e).split(":")[1]
+        return jsonify({"error": msg})
+    except Exception as e:
+        return jsonify({"error": "invalid data"})
+
+    output = {
+        'username': user.username,
+        'user_id': user.user_id,
+        'name': user.name,
+        'email': user.email
+    }
+    token = jwt.encode({'user_id': user.user_id}, app.config.get('SECRET_KEY'), algorithm="HS256")
+    return jsonify({"message": "user created!", "user": output, "token":token})
+
+
 
 
